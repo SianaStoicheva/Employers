8854beee3937eaa37de3a4d0d7c416c27c30bef0
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-04-24 14:49:54 +0300

    Zadacha

---
+++ Empl/Employers.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empl
+{
+    internal class Employers
+    {
+        private string name;
+        private double salary;
+        private string position;
+        private string filed;
+        private string? email ="n/a";
+        private int age = -1;
+
+        public string Name
+        { 
+            get 
+            { return name; 
+            } 
+            set 
+            { 
+                name = value;
+            } 
+        }
+        public double Salary
+        { 
+            get 
+            { 
+                return salary; 
+            } 
+            set
+            {
+                salary = value;
+            } 
+        }
+        public string Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+            }
+        }
+        public string Filed
+        {
+            get
+            {
+                return filed;
+            }
+            set
+            {
+                filed = value; 
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public Employers(string name,double salary,string filed,string? email, string? email1, int age)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Filed = filed;
+            if(email!=null)this.Email = email;
+            if (age != 0) this.Age = age;
+        }
+        public Employers()
+        {
+
+        }
+
+        public void PrintMe()
+        {
+            Console.WriteLine($"Name: {name}, Salary: {salary}, Email: {email}, Age: {age}");
+        }
+    }
+}
---
+++ Empl/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Empl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kolko slujiteli vavejdash: ");
+            int count = int.Parse(Console.ReadLine());
+
+            List<Employers> employers = new List<Employers>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                string[] inputS = input.Split().ToArray();
+
+                string name = inputS[0];//Console.ReadLine();
+                double salary = double.Parse(inputS[1]);
+                string position = inputS[2];
+                string filed = inputS[3];
+                string email = null;
+                if (inputS.Length > 4)
+                {
+                     email = inputS[4];
+                }
+                int age = 0;
+                if (inputS.Length > 5)
+                {
+                     age = int.Parse(inputS[5]);
+                }
+
+                Employers currentEmpl = new Employers(name,salary,position,filed,email,age);
+                
+                employers.Add(currentEmpl);
+            }
+
+            PrintAllEmployers(employers);
+        }
+
+        private static void PrintAllEmployers(List<Employers> employers)
+        {
+            foreach (Employers item in employers)
+            {
+                item.PrintMe();
+            }
+        }
+
+    }
+}
